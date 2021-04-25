21b8b16bf0382f66a40c1fe37ed0af8bc3aa74f3
TheRickster365 <83165129+TheRickster365@users.noreply.github.com>
2021-04-25 21:21:54 +1000

    Add files via upload

--- SMA_WebBox.py
+++ SMA_WebBox.py
@@ -5,30 +5,38 @@ from time import sleep
 import sys
 import json
 
+#These RPC calls below allow you to query the webbox
+#Get Plant overview ie current/daily/total power from inverter
+GetPlantOverview = '{"version":"1.0","proc":"GetPlantOverview","id":"1","format":"JSON"}'
+
+#Gets list of SMA devices connected to RS-485 Bus
+GetDevices = '{"version" : "1.0","proc" : "GetDevices","id" : "1","format" : "JSON"}'
+
+#Get list of Channels from a device
+#GetSensor has a replaceable string --DEVICE-- to be replaced with the device you wish to query
+GetSensor = '{"version":"1.0","proc":"GetProcessData","id":"1","format":"JSON","params":{"devices":[{"key":"--DEVICE--","channels":null}]}}'
+
 # These are examples of data returned from the various Sunny Sensors
 #Sunny Sensor {Pannel Temp / Ambiemt Temp / Irradiation / Wind Speed}
 #https://files.sma.de/downloads/Sensorbox-IEN100914.pdf
-#dev1 = '{"format":"JSON","result":{"devices":[{"key":"SENS0700:17403","channels":[{"unit":"W/m^2","meta":"ExlSolIrr","name":"ExlSolIrr","value":"0"},{"unit":"W/m^2","meta":"IntSolIrr","name":"IntSolIrr","value":"0"},{"unit":"h","meta":"SMA-h-On","name":"SMA-h-On","value":"72091.2252117420"},{"unit":"°C","meta":"TmpAmb C","name":"TmpAmb C","value":"19.23"},{"unit":"°C","meta":"TmpMdul C","name":"TmpMdul C","value":"18.13"},{"unit":"m/s","meta":"WindVel m/s","name":"WindVel m/s","value":"3.3"}]}]},"proc":"GetProcessData","version":"1.0","id":"3"}'
+#'{"format":"JSON","result":{"devices":[{"key":"SENS0700:17403","channels":[{"unit":"W/m^2","meta":"ExlSolIrr","name":"ExlSolIrr","value":"0"},{"unit":"W/m^2","meta":"IntSolIrr","name":"IntSolIrr","value":"0"},{"unit":"h","meta":"SMA-h-On","name":"SMA-h-On","value":"72091.2252117420"},{"unit":"°C","meta":"TmpAmb C","name":"TmpAmb C","value":"19.23"},{"unit":"°C","meta":"TmpMdul C","name":"TmpMdul C","value":"18.13"},{"unit":"m/s","meta":"WindVel m/s","name":"WindVel m/s","value":"3.3"}]}]},"proc":"GetProcessData","version":"1.0","id":"3"}'
 
 #School Meter Box {Counts S/0 signal from Energy Meter}
 #https://files.sma.de/downloads/METERBOX-IEN110611.pdf
 #I use an LDR placed over the flashing LED on my meter to count KW/H pulses
-#dev2 = '{"format":"JSON","result":{"devices":[{"key":"SMBAU008:154002012","channels":[{"unit":"h","meta":"m_OpTm","name":"m_OpTm","value":"43726"},{"unit":"W","meta":"m_Pac","name":"m_Pac","value":"0"},{"unit":"","meta":"m_Reset_Count","name":"m_Reset_Count","value":"21"},{"unit":"","meta":"m_S0 Count","name":"m_S0 Count","value":"70667899"},{"unit":"kWh","meta":"m_S0 kWh","name":"m_S0 kWh","value":"37212.520"}]}]},"proc":"GetProcessData","version":"1.0","id":"4"}'
-#dev3 = '{"format":"JSON","result":{"devices":[{"key":"SMBAU008:154002091","channels":[{"unit":"h","meta":"m_OpTm","name":"m_OpTm","value":"43727"},{"unit":"W","meta":"m_Pac","name":"m_Pac","value":"0"},{"unit":"","meta":"m_Reset_Count","name":"m_Reset_Count","value":"23"},{"unit":"","meta":"m_S0 Count","name":"m_S0 Count","value":"2326375"},{"unit":"kWh","meta":"m_S0 kWh","name":"m_S0 kWh","value":"5043.970"}]}]},"proc":"GetProcessData","version":"1.0","id":"5"}'
+#'{"format":"JSON","result":{"devices":[{"key":"SMBAU008:154002012","channels":[{"unit":"h","meta":"m_OpTm","name":"m_OpTm","value":"43726"},{"unit":"W","meta":"m_Pac","name":"m_Pac","value":"0"},{"unit":"","meta":"m_Reset_Count","name":"m_Reset_Count","value":"21"},{"unit":"","meta":"m_S0 Count","name":"m_S0 Count","value":"70667899"},{"unit":"kWh","meta":"m_S0 kWh","name":"m_S0 kWh","value":"37212.520"}]}]},"proc":"GetProcessData","version":"1.0","id":"4"}'
+#'{"format":"JSON","result":{"devices":[{"key":"SMBAU008:154002091","channels":[{"unit":"h","meta":"m_OpTm","name":"m_OpTm","value":"43727"},{"unit":"W","meta":"m_Pac","name":"m_Pac","value":"0"},{"unit":"","meta":"m_Reset_Count","name":"m_Reset_Count","value":"23"},{"unit":"","meta":"m_S0 Count","name":"m_S0 Count","value":"2326375"},{"unit":"kWh","meta":"m_S0 kWh","name":"m_S0 kWh","value":"5043.970"}]}]},"proc":"GetProcessData","version":"1.0","id":"5"}'
 
 #SMA Inverter
-#dev4 = '{"format":"JSON","result":{"devices":[{"key":"WR6KA-05:2001124007","channels":[{"unit":"","meta":"Balancer","name":"Balancer","value":""},{"unit":"kWh","meta":"E-Total","name":"E-Total","value":""},{"unit":"Hz","meta":"Fac","name":"Fac","value":""},{"unit":"","meta":"Fehler","name":"Fehler","value":""},{"unit":"h","meta":"h-On","name":"h-On","value":""},{"unit":"h","meta":"h-Total","name":"h-Total","value":""},{"unit":"A","meta":"Iac-Ist","name":"Iac-Ist","value":""},{"unit":"A","meta":"Ipv","name":"Ipv","value":""},{"unit":"","meta":"Netz-Ein","name":"Netz-Ein","value":""},{"unit":"W","meta":"Pac","name":"Pac","value":""},{"unit":"kOhm","meta":"Riso","name":"Riso","value":""},{"unit":"","meta":"Seriennummer","name":"Seriennummer","value":""},{"unit":"","meta":"Status","name":"Status","value":""},{"unit":"V","meta":"Uac","name":"Uac","value":""},{"unit":"V","meta":"Upv-Ist","name":"Upv-Ist","value":""},{"unit":"V","meta":"Upv-Soll","name":"Upv-Soll","value":""},{"unit":"Ohm","meta":"Zac","name":"Zac","value":""}]}]},"proc":"GetProcessData","version":"1.0","id":"6"}'
+#'{"format":"JSON","result":{"devices":[{"key":"WR6KA-05:2001124007","channels":[{"unit":"","meta":"Balancer","name":"Balancer","value":""},{"unit":"kWh","meta":"E-Total","name":"E-Total","value":""},{"unit":"Hz","meta":"Fac","name":"Fac","value":""},{"unit":"","meta":"Fehler","name":"Fehler","value":""},{"unit":"h","meta":"h-On","name":"h-On","value":""},{"unit":"h","meta":"h-Total","name":"h-Total","value":""},{"unit":"A","meta":"Iac-Ist","name":"Iac-Ist","value":""},{"unit":"A","meta":"Ipv","name":"Ipv","value":""},{"unit":"","meta":"Netz-Ein","name":"Netz-Ein","value":""},{"unit":"W","meta":"Pac","name":"Pac","value":""},{"unit":"kOhm","meta":"Riso","name":"Riso","value":""},{"unit":"","meta":"Seriennummer","name":"Seriennummer","value":""},{"unit":"","meta":"Status","name":"Status","value":""},{"unit":"V","meta":"Uac","name":"Uac","value":""},{"unit":"V","meta":"Upv-Ist","name":"Upv-Ist","value":""},{"unit":"V","meta":"Upv-Soll","name":"Upv-Soll","value":""},{"unit":"Ohm","meta":"Zac","name":"Zac","value":""}]}]},"proc":"GetProcessData","version":"1.0","id":"6"}'
 
 #Sunny Webbox Plant Overview
 #https://www.solar-electric.com/lib/wind-sun/SunnyWebbox-manual.pdf
 #https://www.sunnyportal.com/Documents/SPortal-WB-CC-BA-US_en-27.pdf
-#dev5 = '{"format":"JSON","result":{"overview":[{"unit":"W","meta":"GriPwr","name":"GriPwr","value":"0"},{"unit":"kWh","meta":"GriEgyTdy","name":"GriEgyTdy","value":"12.384"},{"unit":"kWh","meta":"GriEgyTot","name":"GriEgyTot","value":"67812.215"},{"unit":"","meta":"OpStt","name":"OpStt","value":""},{"unit":"","meta":"Msg","name":"Msg","value":""}]},"proc":"GetPlantOverview","version":"1.0","id":"7"}'
+#'{"format":"JSON","result":{"overview":[{"unit":"W","meta":"GriPwr","name":"GriPwr","value":"0"},{"unit":"kWh","meta":"GriEgyTdy","name":"GriEgyTdy","value":"12.384"},{"unit":"kWh","meta":"GriEgyTot","name":"GriEgyTot","value":"67812.215"},{"unit":"","meta":"OpStt","name":"OpStt","value":""},{"unit":"","meta":"Msg","name":"Msg","value":""}]},"proc":"GetPlantOverview","version":"1.0","id":"7"}'
 
-GetPlantOverview = '{"version":"1.0","proc":"GetPlantOverview","id":"1","format":"JSON"}'
-GetSensor = '{"version":"1.0","proc":"GetProcessData","id":"1","format":"JSON","params":{"devices":[{"key":"--DEVICE--","channels":null}]}}'
-GetDevices = '{"version" : "1.0","proc" : "GetDevices","id" : "1","format" : "JSON"}'
 
-#Generate a transmit socket object
+#Generate a UDP transmit socket object
 txSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 dataready = False
@@ -167,7 +175,7 @@ def rxThread(portNum):
     global result
     global dataready
     
-    #Generate a UDP socket
+    #Generate a UDP receive socket
     rxSocket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
                              
     #Bind to any available address on port *portNum*
@@ -234,3 +242,5 @@ def main(args):
 
     exit = True
    
+if __name__=="__main__":
+    main(sys.argv[1:0]) 
